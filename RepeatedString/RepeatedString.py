9f47d48a478c3571b23ea6e3a51b84ebfd973f1a
AChurakov <alexchurakov@Designs-MacBook-Pro.local>
2016-04-15 23:54:04 +0300

    fix bugs
    sorry about shit vims files

--- RepeatedString/RepeatedString.py
+++ RepeatedString/RepeatedString.py
@@ -1,16 +1,14 @@
 def get_most_com_sub(s):
-        sub = {}
-        max_value = 0
-        for i in range(len(s)):
-            count = 0
-            sub_value  = s[i]
-            while i+1 < len(s) and s[i] <= s[i+1]:
-                count += 1
-                i += 1
-                sub_value += s[i]
-            sub[count] = sub.get(count, sub_value)
-            max_value = max(sub.keys())
-        return s.count(sub[max_value])
+        n = len(s)
+        i = 2
+        k = 0
+        while i < n:
+            if n % i == 0 and n / i == s.count(s[:i]) and s.count(s[:i]) > k:
+                k = s.count(s[:i])
+                t = i
+            i += 1
+        return k
+
 
 
 print(get_most_com_sub(input()))
